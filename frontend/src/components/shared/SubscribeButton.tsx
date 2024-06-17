1b87d19d247e3616615db2a0c77709275099db27
tinarao <golovindanila1613@gmail.com>
2024-06-18 01:25:41 +0300

    made subscriptions

--- frontend/src/components/shared/SubscribeButton.tsx
+++ frontend/src/components/shared/SubscribeButton.tsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from 'react';
 import { useSubscriptions } from '@/hooks/useSubscribes';
 import { UserPlus2, UserRoundX } from 'lucide-react';
 import { toast } from 'sonner';
+import { api } from '@/lib/rpc';
+import { useNavigate } from '@tanstack/react-router';
+import { queryClient } from '@/main';
 
 interface SBProps {
   currentUser?: User;
@@ -12,7 +15,9 @@ interface SBProps {
 
 const SubscribeButton = ({ currentUser, targetUser }: SBProps) => {
   const [isSubscribed, setIsSubscribed] = useState(false);
-  const { subscriptionIDs } = useSubscriptions();
+  const [isTheSameUser, setIsTheSameUser] = useState(false);
+  const { subscriptionIDs, subscribe, unsubscribe } = useSubscriptions();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!currentUser) {
@@ -20,16 +25,53 @@ const SubscribeButton = ({ currentUser, targetUser }: SBProps) => {
       return;
     }
 
+    if (currentUser.id === targetUser.id) {
+      setIsTheSameUser(true);
+      return;
+    }
+
     const isSubscribedTo = subscriptionIDs.includes(targetUser.id);
     setIsSubscribed(isSubscribedTo);
-  }, [currentUser, subscriptionIDs, targetUser.id]);
+  }, [currentUser, targetUser.id, subscriptionIDs]);
 
   const unsubscribeHandler = async () => {
-    toast.success('Отписан');
+    if (!currentUser) {
+      navigate({ to: '/login' });
+      return;
+    }
+
+    const res = await api.users.unsubscribe[':targetUserID{[0-9]+}'].$patch({
+      param: { targetUserID: String(targetUser.id) },
+    });
+    if (!res.ok) {
+      const resp = await res.text();
+      return toast.error(resp);
+    }
+
+    unsubscribe(targetUser.id);
+    queryClient.invalidateQueries({ queryKey: ['get-user-by-username'] });
+    console.log(await res.json());
+    return;
   };
 
   const subscribeHandler = async () => {
-    toast.success('Подписан');
+    if (!currentUser) {
+      navigate({ to: '/login' });
+      return;
+    }
+
+    const res = await api.users.subscribe[':targetUserID{[0-9]+}'].$patch({
+      param: { targetUserID: String(targetUser.id) },
+    });
+    if (!res.ok) {
+      const resp = await res.text();
+      return toast.error(resp);
+    }
+
+    subscribe(targetUser.id);
+    queryClient.invalidateQueries({ queryKey: ['get-user-by-username'] });
+    console.log(await res.json());
+    return;
   };
 
   // TODO: Функционал подписок завозить сюда
@@ -37,6 +79,7 @@ const SubscribeButton = ({ currentUser, targetUser }: SBProps) => {
     <>
       {isSubscribed ? (
         <Button
+          disabled={isTheSameUser}
           onClick={unsubscribeHandler}
           variant="outline"
           className="hover:bg-rose-400 hover:text-white"
@@ -45,6 +88,7 @@ const SubscribeButton = ({ currentUser, targetUser }: SBProps) => {
         </Button>
       ) : (
         <Button
+          disabled={isTheSameUser}
           onClick={subscribeHandler}
           variant="outline"
           className="hover:bg-green-300"
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -55,9 +55,53 @@ export const usersRoute = new Hono()
 
         return c.json(updated)
     })
-    .patch("/subscribe/:subscriberID{[0-9]+}/:targetUserID{[0-9]+}", auth, async c => {
+    .patch("/unsubscribe/:targetUserID{[0-9]+}", auth, async c => {
         const user = c.var.user;
-        const subscriberID = parseInt(c.req.param('subscriberID'));
+        const targetUserID = parseInt(c.req.param('targetUserID'));
+
+        try {
+            const updatedTargetUser = await prisma.$transaction(async tx => {
+                const target = await tx.user.findFirst({
+                    where: { id: targetUserID },
+                    include: { subscribers: { select: { id: true } } }
+                })
+                if (!target) throw new Error("target-not-found");
+
+                let isSubscribed: boolean = false;
+                target.subscribers.forEach(i => {
+                    if (i.id === user.id) {
+                        isSubscribed = true;
+                    }
+                })
+                if (!isSubscribed) throw new Error('not-subscribed')
+
+                const updatedTarget = await tx.user.update({
+                    where: { id: target.id },
+                    data: { subscribers: { disconnect: { id: user.id } } }
+                })
+
+                await tx.user.update({
+                    where: { id: user.id },
+                    data: { subscribedTo: { disconnect: { id: target.id } } }
+                })
+
+                return updatedTarget
+            })
+
+            return c.json(updatedTargetUser, 201)
+        } catch (error) {
+            switch ((error as Error).message) {
+                case "target-not-found":
+                    return c.text("Пользователь не найден", 404);
+                case "not-subscribed":
+                    return c.text("Вы не подписаны на этого пользователя!", 400);
+                default:
+                    return c.text("Внутренняя ошибка сервера", 500)
+            }
+        }
+    })
+    .patch("/subscribe/:targetUserID{[0-9]+}", auth, async c => {
+        const user = c.var.user;
         const targetUserID = parseInt(c.req.param('targetUserID'));
 
         try {
@@ -82,7 +126,7 @@ export const usersRoute = new Hono()
                 return updatedTarget;
             })
 
-            return c.json(updatedTargetUser)
+            return c.json(updatedTargetUser, 201)
         } catch (error) {
             switch ((error as Error).message) {
                 case "target-not-found":
